6f9716028daa99ce98bd476b1bbb12f40e22c32f
Shiva Aryal <shivaaryal62@gmail.com>
2024-04-26 13:36:51 +0545

    email not avaliable function implemented

--- src/users/dto/user-signup.dto.ts
+++ src/users/dto/user-signup.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength, isString } from "class-validator";
 
-export class UserSignUp{
+export class UserSignUpDto{
     @IsNotEmpty({message:'Name can not be Null'})
     @IsString({message:'Name should be string'})
     name:string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
-import { UserSignUp } from './dto/user-signup.dto';
+import { UserSignUpDto } from './dto/user-signup.dto';
 
 @Injectable()
 export class UsersService {
@@ -14,8 +14,11 @@ export class UsersService {
     private usersRepository: Repository<UserEntity>,
   ) {}
 
-  async signup(userSignUp:UserSignUp):Promise<UserEntity>{
-    const user=this .usersRepository.create(userSignUp);
+  async signup(userSignUpDto:UserSignUpDto):Promise<UserEntity>{
+    const userExist=await this.findUserByEmail(userSignUpDto.email)
+    if(userExist) throw new BadRequestException('Email is not available.')
+
+    const user=this .usersRepository.create(userSignUpDto);
     return await this.usersRepository.save(user);
   }
   
@@ -37,5 +40,9 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async findUserByEmail(email:string){
+    return await this.usersRepository.findOneBy({email});
   }
 }
