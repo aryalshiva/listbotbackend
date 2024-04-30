c25e69123ed35dc982bbb8f4bbb59d1908ecc99a
Shiva Aryal <shivaaryal62@gmail.com>
2024-04-30 12:55:04 +0545

    changes to signup.dto by extend signin cause same code

--- src/users/dto/user-signup.dto.ts
+++ src/users/dto/user-signup.dto.ts
@@ -1,15 +1,10 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength, isString } from "class-validator";
+import { UserSignInDto } from "./user-signin.dto";
 
-export class UserSignUpDto{
+export class UserSignUpDto extends UserSignInDto{
     @IsNotEmpty({message:'Name can not be Null'})
     @IsString({message:'Name should be string'})
     name:string;
 
-    @IsNotEmpty({message :'email can not be empty '})
-    @IsEmail({},{message :'Please provide a valid email '})
-    email:string;
 
-    @IsNotEmpty({message :'Password can not be empty '})
-    @MinLength(5,{message :'Password minimum character should be 5.'})
-    password:string;
 }
