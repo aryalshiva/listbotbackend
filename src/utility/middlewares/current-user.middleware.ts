e0dae73feb226db7cd13644827e1224d59cf5cce
Shiva Aryal <shivaaryal62@gmail.com>
2024-05-01 16:38:52 +0545

    combine the current user to request

--- src/utility/middlewares/current-user.middleware.ts
+++ src/utility/middlewares/current-user.middleware.ts
@@ -1,9 +1,19 @@
 
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { isArray } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+
+declare global{
+    namespace Express{
+        interface Request{
+            currentUser?:UserEntity;
+        }
+    }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -11,13 +21,13 @@ constructor(private readonly UserService:UsersService){}
     async use(req: Request, res: Response, next: NextFunction) {
     const authHeader=req.headers.authorization || req.headers.Authorization ;
     if(!authHeader || isArray(authHeader)|| !authHeader.startsWith('Bearer ')){
-        //req.currentUser=null;
+        req.currentUser=null;
         next();
     }else{
         const token=authHeader.split(' ')[1];
         const{id}=<JwtPayLoad>verify(token,process.env.ACCESS_TOKEN_SECRET_KEY);
         const currentUser=await this.UserService.findOne(+id);
-        console.log(currentUser);
+        req.currentUser=currentUser;
         next();
     }
   }
